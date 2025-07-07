49d2dd22a749397b1fb318f9b480731df9002ada
Luis Lima <luisfernando.lima2005@gmail.com>
2025-07-07 18:49:33 -0300

    Introdução ao Shell

---
+++ introducao.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Entrando no diretório raiz\n"
+cd /
+
+echo "Em qual diretório estou?\n"
+pwd
+
+echo "Listando arquivos do diretorio\n"
+ls
+
+
+echo "Meu nome é Luis!
+Tudo bem com você?"
+
+
+echo \
+"Gosto muito de Linux!
+E você?"
